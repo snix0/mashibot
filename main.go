da934dd26677d226a5af02e7c78f44039dbd3663
Andy Feng <ayfeng@uci.edu>
2020-07-03 18:14:51 -0700

    Fix inconsistent spacing

--- main.go
+++ main.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+    "flag"
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
     "net/http"
     "encoding/json"
     "io/ioutil"
     "strings"
 
-	"github.com/bwmarrin/discordgo"
+    "github.com/bwmarrin/discordgo"
 )
 
 // Variables used for command line parameters
 var (
-	Token string
+    Token string
 )
 
 func init() {
 
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.Parse()
+    flag.StringVar(&Token, "t", "", "Bot Token")
+    flag.Parse()
 }
 
 func main() {
 
-	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
-	}
+    // Create a new Discord session using the provided bot token.
+    dg, err := discordgo.New("Bot " + Token)
+    if err != nil {
+        fmt.Println("error creating Discord session,", err)
+        return
+    }
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+    // Register the messageCreate func as a callback for MessageCreate events.
+    dg.AddHandler(messageCreate)
 
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
-	}
+    // Open a websocket connection to Discord and begin listening.
+    err = dg.Open()
+    if err != nil {
+        fmt.Println("error opening connection,", err)
+        return
+    }
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+    // Wait here until CTRL-C or other term signal is received.
+    fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
 
-	// Cleanly close down the Discord session.
-	dg.Close()
+    // Cleanly close down the Discord session.
+    dg.Close()
 }
 
 // This function will be called (due to AddHandler above) every time a new
@@ -81,16 +81,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
     word = strings.ReplaceAll(word, "]", "")
     word = strings.ReplaceAll(word, "\"", "")
 
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	if m.Content == "!meme" {
-		s.ChannelMessageSend(m.ChannelID, url)
-	}
+    // Ignore all messages created by the bot itself
+    // This isn't required in this specific example but it's a good practice.
+    if m.Author.ID == s.State.User.ID {
+        return
+    }
+    if m.Content == "!meme" {
+        s.ChannelMessageSend(m.ChannelID, url)
+    }
 
-	if m.Content == "Do you like The Quarter Game" {
-		s.ChannelMessageSend(m.ChannelID, "YESS! The Quarter Game is better than " + word + "!!!")
-	}
+    if m.Content == "Do you like The Quarter Game" {
+        s.ChannelMessageSend(m.ChannelID, "YESS! The Quarter Game is better than " + word + "!!!")
+    }
 }
